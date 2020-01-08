bee8f2ebe896b715b5b2f33759d2420d7027a614
dingliufan <358892638@qq.com>
2020-01-08 17:29:10 +0800

    修改融云key

--- app/src/main/java/com/mebooth/text/Application.java
+++ app/src/main/java/com/mebooth/text/Application.java
@@ -27,7 +27,7 @@ public class Application extends AppApplication {
         UserTokenJson userTokenJson = new UserTokenJson();
         userTokenJson.setUserid("12358333");
         userTokenJson.setFrom("14");
-        userTokenJson.setToken("9K1Dp0NMTdw3Ytl3");
+        userTokenJson.setToken("e4nPt7XMFzwkGyQf");
         userTokenJson.setVersion("4.8.0");
         this.setUserTokenJson(userTokenJson);
         if (appApplication != null){
